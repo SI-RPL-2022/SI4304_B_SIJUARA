c950009fe6c2ed65e7e553b50a3bac56a55a7d1e
Shazkia Septiaweni <87314321+shazkiaseptiaweni@users.noreply.github.com>
2022-04-18 09:56:01 +0700

    Mengubah tampilan profil admin #7

--- sijuara/resources/views/profile/index.blade.php
+++ sijuara/resources/views/profile/index.blade.php
@@ -4,7 +4,7 @@
     <div class="profile">
         <div class="sm:flex sm:items-center sm:justify-between mb-6">
             <div>
-                <h2 class="text-gray-700 uppercase font-bold">Profile</h2>
+                <h2 class="text-gray-1000 uppercase font-bold">Profile</h2>
             </div>
             <div class="flex flex-wrap items-center">
                 <a href="{{ route('profile.edit') }}" class="bg-gray-200 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
@@ -13,7 +13,7 @@
                 </a>
                 <a href="{{ route('profile.change.password') }}" class="bg-gray-200 text-gray-700 text-sm uppercase ml-2 py-2 px-4 flex items-center rounded">
                     <svg class="w-3 h-3 fill-current" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" class="svg-inline--fa fa-plus fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg>
-                    <span class="ml-2 text-xs font-semibold">Change Password</span>
+                    <span class="ml-2 text-xs font-semibold">Ubah Password</span>
                 </a>
             </div>
         </div>
