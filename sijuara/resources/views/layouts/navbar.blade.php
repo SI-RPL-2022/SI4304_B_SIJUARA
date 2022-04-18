9f5af319b51d047810b035b9e1efe3677aae025f
tiarahatig <tiarahatigiwangkara@gmail.com>
2022-04-18 09:38:12 +0700

    Merubah tampilan navbar #7
    sudah melakukan perubahan tampilan pada navbar

--- sijuara/resources/views/layouts/navbar.blade.php
+++ sijuara/resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-red-700 px-4 sm:px-6 py-3 flex items-center justify-between shadow h-16 fixed top-0 left-0 right-0 z-50">
+<div class="bg-purple-700 px-4 sm:px-6 py-3 flex items-center justify-between shadow h-16 fixed top-0 left-0 right-0 z-50">
     <div class="flex items-center text-white">
         <svg class="fill-current h-6 w-6 mr-2 feather feather-book-open" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path></svg>
         <span class="font-semibold text-sm sm:text-xl tracking-tight">SIJUARA</span>
@@ -10,7 +10,7 @@
                 <p class="text-sm text-white font-semibold leading-none">{{ auth()->user()->name }}</p>
                 <svg class="w-4 h-4 stroke-current text-gray-200 ml-1 feather feather-chevron-down" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><polyline points="6 9 12 15 18 9"></polyline></svg>
             </div>
-            <div class="bg-red-700 absolute top-0 right-0 mt-12 -mr-6 shadow rounded-bl rounded-br">
+            <div class="bg-purpel-700 absolute top-0 right-0 mt-12 -mr-6 shadow rounded-bl rounded-br">
                 <div class="hidden h-24 w-48" id="navdropdown">
                     <div class="px-8 py-4 border-t border-red-800">
                         <a href="{{ route('profile') }}" class="flex items-center pb-3 text-sm text-gray-200 font-semibold">
