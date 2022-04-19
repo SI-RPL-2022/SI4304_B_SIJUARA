e240598f4d4ec2607f2cf864c713780ac1a97de6
yadinugrahaa-github <skyres9@gmail.com>
2022-04-19 22:48:32 +0700

    Menampilkan data dari database di view mata pelajaran #3
    Data dari database sudah bisa ditampilkan

--- sijuara/resources/views/backend/subjects/index.blade.php
+++ sijuara/resources/views/backend/subjects/index.blade.php
@@ -14,6 +14,7 @@
                 </a>
             </div>
         </div>
+        @foreach ($subjects as $subject)
         <div class="mt-8 bg-white rounded border-b-4 border-gray-300">
             <div class="flex flex-wrap items-center uppercase text-sm font-semibold bg-pink-400 text-dark-600 rounded-tl rounded-tr">
                 <div class="w-1/12 px-4 py-3">#</div>
@@ -25,10 +26,10 @@
             </div>
      
                 <div class="flex flex-wrap items-center text-gray-700 border-t-2 border-l-4 border-r-4 border-gray-300">
-                    <div class="w-3/12 px-4 py-3 text-sm font-semibold text-gray-600 tracking-tight"></div>
-                    <div class="w-2/12 px-4 py-3 text-sm font-semibold text-gray-600 tracking-tight"></div>
-                    <div class="w-2/12 px-4 py-3 text-sm font-semibold text-gray-600 tracking-tight"></div>
-                    <div class="w-3/12 px-4 py-3 text-sm text-gray-600 tracking-tight"></div>
+                    <div class="w-3/12 px-4 py-3 text-sm font-semibold text-gray-600 tracking-tight">{{ $subject->name }}</div>
+                    <div class="w-2/12 px-4 py-3 text-sm font-semibold text-gray-600 tracking-tight">{{ $subject->subject_code }}</div>
+                    <div class="w-2/12 px-4 py-3 text-sm font-semibold text-gray-600 tracking-tight">{{ $subject->teacher->user->name }}</div>
+                    <div class="w-3/12 px-4 py-3 text-sm text-gray-600 tracking-tight">{{ $subject->description }}</div>
                     <div class="w-2/12 flex items-center justify-end px-3">
                         <a href="">
                                 <button type="submit" class="bg-blue-600 block p-1 border border-blue-600 rounded-sm">
@@ -36,15 +37,18 @@
                                 </button>
                         </a>
                         <form action="" method="POST" class="inline-flex ml-1">
+                            @csrf
+                            @method('DELETE')
                             <button type="submit" class="bg-red-600 block p-1 border border-red-600 rounded-sm">
                                     <svg class="h-3 w-3 fill-current text-gray-100" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" class="svg-inline--fa fa-trash fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path></svg>
                             </button>
                         </form>
                     </div>
                 </div>
-        
+        @endforeach
         </div>
         <div class="mt-8">
+            {{ $subjects->links() }}
         </div>
     </div>
 @endsection
