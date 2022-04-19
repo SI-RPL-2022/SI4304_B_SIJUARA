3e218712a9a98f36882b3e2ca42cf9b349849dd6
yadinugrahaa-github <skyres9@gmail.com>
2022-04-19 21:15:28 +0700

    Membuat tampilan Dashboard admin
    data masih belum muncul

---
+++ sijuara/resources/views/dashboard/admin.blade.php
@@ -0,0 +1,21 @@
+<div class="w-full block mt-8">
+    <div class="flex flex-wrap sm:flex-no-wrap justify-between">
+        <div>
+            <h3 class="text-gray-700 uppercase font-bold">
+                <span class="text-4xl"></span>
+                <span class="leading-tight">Orang Tua</span>
+        </div>
+        <div>
+            <h3 class="text-gray-700 uppercase font-bold">
+                <span class="text-4xl"></span>
+                <span class="leading-tight">Guru</span>
+            </h3>
+        </div>
+        <div>
+            <h3 class="text-gray-700 uppercase font-bold">
+                <span class="text-4xl"></span>
+                <span class="leading-tight">Siswa</span>
+            </h3>
+        </div>
+    </div>
+</div>
