538b3f06af13dc805ae4c60faa2bf4a56298468d
tiarahatig <tiarahatigiwangkara@gmail.com>
2022-04-19 22:43:28 +0700

    Mengubah table kelas dan mata pelajaran #4 #3

--- sijuara/resources/views/backend/subjects/index.blade.php
+++ sijuara/resources/views/backend/subjects/index.blade.php
@@ -8,18 +8,19 @@
                 <h2 class="text-gray-700 uppercase font-bold">Mata Pelajaran</h2>
             </div>
             <div class="flex flex-wrap items-center">
-                <a href="" class="bg-gray-200 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
+                <a href="" class="bg-green-500 text-white-50 text-sm uppercase py-2 px-4 flex items-center rounded">
                     <svg class="w-3 h-3 fill-current" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" class="svg-inline--fa fa-plus fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg>
                     <span class="ml-2 text-xs font-semibold">Mata Pelajaran</span>
                 </a>
             </div>
         </div>
         <div class="mt-8 bg-white rounded border-b-4 border-gray-300">
-            <div class="flex flex-wrap items-center uppercase text-sm font-semibold bg-gray-300 text-gray-600 rounded-tl rounded-tr">
-                <div class="w-3/12 px-4 py-3">Nama</div>
-                <div class="w-2/12 px-4 py-3">Kode</div>
-                <div class="w-2/12 px-4 py-3">Guru</div>
-                <div class="w-3/12 px-4 py-3">Keterangan</div>
+            <div class="flex flex-wrap items-center uppercase text-sm font-semibold bg-pink-400 text-dark-600 rounded-tl rounded-tr">
+                <div class="w-1/12 px-4 py-3">#</div>
+                <div class="w-2/12 px-4 py-3">Nama</div>
+                <div class="w-1/12 px-4 py-3">Kode</div>
+                <div class="w-4/12 px-4 py-3 text-center">Guru</div>
+                <div class="w-2/12 px-4 py-3">Keterangan</div>
                 <div class="w-2/12 px-4 py-3 text-right">Edit</div>
             </div>
      
