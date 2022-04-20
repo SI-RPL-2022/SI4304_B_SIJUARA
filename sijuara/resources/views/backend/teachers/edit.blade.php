2a7065b0424f3681c6009f1577c473bb0281ca54
tiarahatig <tiarahatigiwangkara@gmail.com>
2022-04-21 00:08:36 +0700

    update backend guru #5

--- sijuara/resources/views/backend/teachers/edit.blade.php
+++ sijuara/resources/views/backend/teachers/edit.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="roles">
+    <div class="roles">
 
         <div class="flex items-center justify-between mb-6">
             <div>
-                <h2 class="text-gray-700 uppercase font-bold">Edit Teacher</h2>
+                <h2 class="text-gray-700 uppercase font-bold">Edit Guru Pengampu</h2>
             </div>
             <div class="flex flex-wrap items-center">
                 <a href="{{ route('teacher.index') }}" class="bg-gray-200 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
                     <svg class="w-3 h-3 fill-current" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-left" class="svg-inline--fa fa-long-arrow-alt-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"></path></svg>
-                    <span class="ml-2 text-xs font-semibold">Back</span>
+                    <span class="ml-2 text-xs font-semibold">Kembali</span>
                 </a>
             </div>
         </div>
 
         <div class="table w-full mt-8 bg-white rounded">
             <form action="{{ route('teacher.update',$teacher->id) }}" method="POST" class="w-full max-w-xl px-6 py-12" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
 
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3"></div>
@@ -27,14 +29,14 @@
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Name
+                            Nama
                         </label>
                     </div>
                     <div class="md:w-2/3">
                         <input name="name" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $teacher->user->name }}">
-                  
-                            <p class="text-red-500 text-xs italic"></p>
-     
+                        @error('name')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
                 <div class="md:flex md:items-center mb-6">
@@ -45,24 +47,28 @@
                     </div>
                     <div class="md:w-2/3">
                         <input name="email" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="email" value="{{ $teacher->user->email }}">
-                            <p class="text-red-500 text-xs italic"></p>
+                        @error('email')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Phone
+                            No HP
                         </label>
                     </div>
                     <div class="md:w-2/3">
                         <input name="phone" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $teacher->phone }}">
-                            <p class="text-red-500 text-xs italic"></p>
+                        @error('phone')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Gender
+                            Jenis Kelamin
                         </label>
                     </div>
                     <div class="md:w-2/3">
@@ -80,47 +86,54 @@
                                 <span class="text-sm">Other</span>
                             </label>
                         </div>
-                            <p class="text-red-500 text-xs italic"></p>
+                        @error('gender')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Date of Birth
+                            Tanggal Lahir
                         </label>
                     </div>
                     <div class="md:w-2/3">
                         <input name="dateofbirth" id="datepicker-te" autocomplete="off" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $teacher->dateofbirth }}">
-                            <p class="text-red-500 text-xs italic"></p>
+                        @error('dateofbirth')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Current Address
+                            Alamat Sekarang
                         </label>
                     </div>
                     <div class="md:w-2/3">
                         <input name="current_address" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $teacher->current_address }}">
-                            <p class="text-red-500 text-xs italic"></p>
-
+                        @error('current_address')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Permanent Address
+                            Alamat Tetap
                         </label>
                     </div>
                     <div class="md:w-2/3">
                         <input name="permanent_address" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $teacher->permanent_address }}">
-                            <p class="text-red-500 text-xs italic"></p>
+                        @error('permanent_address')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Picture :
+                            Gambar :
                         </label>
                     </div>
                     <div class="md:w-2/3">
@@ -132,7 +145,7 @@
                     <div class="md:w-1/3"></div>
                     <div class="md:w-2/3">
                         <button class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
-                            Update Teacher
+                            Update Guru Pengampu
                         </button>
                     </div>
                 </div>
@@ -140,7 +153,9 @@
         </div>
         
     </div>
+@endsection
 
+@push('scripts')
 <script>
     $(function() {       
         $( "#datepicker-te" ).datepicker({ dateFormat: 'yy-mm-dd' });
