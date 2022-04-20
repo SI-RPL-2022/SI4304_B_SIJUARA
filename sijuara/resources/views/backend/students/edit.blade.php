ea0121c9a0cc8dd510c83e216d99ad3d8266510b
tiarahatig <tiarahatigiwangkara@gmail.com>
2022-04-21 01:04:34 +0700

    Fixing section student #2

--- sijuara/resources/views/backend/students/edit.blade.php
+++ sijuara/resources/views/backend/students/edit.blade.php
@@ -10,7 +10,7 @@
             <div class="flex flex-wrap items-center">
                 <a href="{{ route('student.index') }}" class="bg-gray-200 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
                     <svg class="w-3 h-3 fill-current" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-left" class="svg-inline--fa fa-long-arrow-alt-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"></path></svg>
-                    <span class="ml-2 text-xs font-semibold">Back</span>
+                    <span class="ml-2 text-xs font-semibold">Kembali</span>
                 </a>
             </div>
         </div>
@@ -68,7 +68,7 @@
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Nomor HP/WA
+                            Nomor HP
                         </label>
                     </div>
                     <div class="md:w-2/3">
@@ -152,7 +152,7 @@
                     <div class="md:w-2/3 block text-gray-600 font-bold">
                         <div class="relative">
                             <select name="class_id" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                                <option value="">--Select Class--</option>
+                                <option value="">--Pilih Kelas--</option>
                                 @foreach ($classes as $class)
                                     <option value="{{ $class->id }}"
                                         {{ ($class->id === $student->class_id) ? 'selected' : '' }}
@@ -176,7 +176,7 @@
                     <div class="md:w-2/3 block text-gray-600 font-bold">
                         <div class="relative">
                             <select name="parent_id" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                                <option value="">--Select Parent--</option>
+                                <option value="">--Pilih Orangtua--</option>
                                 @foreach ($parents as $parent)
                                     <option value="{{ $parent->id }}"
                                         {{ ($parent->id === $student->parent_id) ? 'selected' : '' }}
@@ -205,7 +205,7 @@
                 <div class="md:flex md:items-center">
                     <div class="md:w-1/3"></div>
                     <div class="md:w-2/3">
-                        <button class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                        <button class="shadow bg-blue-500 hover:bg-purple-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
                             Update Data
                         </button>
                     </div>
--- sijuara/resources/views/backend/students/show.blade.php
+++ sijuara/resources/views/backend/students/show.blade.php
@@ -5,12 +5,16 @@
 
         <div class="flex items-center justify-between mb-6">
             <div>
+<<<<<<< Updated upstream
                 <h2 class="text-gray-700 uppercase font-bold">Profil Siswa</h2>
+=======
+                <h2 class="text-gray-700 uppercase font-bold">Data Detail Siswa</h2>
+>>>>>>> Stashed changes
             </div>
             <div class="flex flex-wrap items-center">
                 <a href="{{ route('student.index') }}" class="bg-gray-200 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
                     <svg class="w-3 h-3 fill-current" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-left" class="svg-inline--fa fa-long-arrow-alt-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"></path></svg>
-                    <span class="ml-2 text-xs font-semibold">Back</span>
+                    <span class="ml-2 text-xs font-semibold">Kembali</span>
                 </a>
             </div>
         </div>
@@ -47,7 +51,11 @@
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+<<<<<<< Updated upstream
                             Nomor Urut Absen :
+=======
+                        Nomor Urut Absen :
+>>>>>>> Stashed changes
                         </label>
                     </div>
                     <div class="md:w-2/3">
@@ -57,7 +65,11 @@
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+<<<<<<< Updated upstream
                             Nomor HP/WA :
+=======
+                            No HP :
+>>>>>>> Stashed changes
                         </label>
                     </div>
                     <div class="md:w-2/3">
@@ -87,7 +99,11 @@
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+<<<<<<< Updated upstream
                             Alamat pada KK :
+=======
+                            Alamat Pada KK :
+>>>>>>> Stashed changes
                         </label>
                     </div>
                     <div class="md:w-2/3">
@@ -117,7 +133,11 @@
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+<<<<<<< Updated upstream
                             Nama Orangtua/Wali :
+=======
+                            Orang Tua Siswa :
+>>>>>>> Stashed changes
                         </label>
                     </div>
                     <div class="md:w-2/3 block text-gray-600 font-bold">
@@ -127,7 +147,11 @@
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+<<<<<<< Updated upstream
                             Email Orangtua/Wali :
+=======
+                            Email Orang Tua :
+>>>>>>> Stashed changes
                         </label>
                     </div>
                     <div class="md:w-2/3 block text-gray-600 font-bold">
@@ -137,7 +161,11 @@
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+<<<<<<< Updated upstream
                             No HP Orangtua/Wali :
+=======
+                            No Orang Tua :
+>>>>>>> Stashed changes
                         </label>
                     </div>
                     <div class="md:w-2/3 block text-gray-600 font-bold">
@@ -147,7 +175,11 @@
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+<<<<<<< Updated upstream
                             Alamat Orangtua :
+=======
+                            Alamat Orang Tua :
+>>>>>>> Stashed changes
                         </label>
                     </div>
                     <div class="md:w-2/3 block text-gray-600 font-bold">
