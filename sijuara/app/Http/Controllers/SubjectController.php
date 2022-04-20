dfd36d69adf480f7645e21de8bb4b3b97b77515b
yadinugrahaa-github <skyres9@gmail.com>
2022-04-20 22:23:14 +0700

    Menambahkan controller CRUD data mata pelajaran #3
    route pada front end masih blm diatur

--- sijuara/app/Http/Controllers/SubjectController.php
+++ sijuara/app/Http/Controllers/SubjectController.php
@@ -21,5 +21,75 @@ class SubjectController extends Controller
         
         return view('backend.subjects.index', compact('subjects'));
     }
-}
 
+
+    public function create()
+    {
+        $teachers = Teacher::latest()->get();
+
+        return view('backend.subjects.create', compact('teachers'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'          => 'required|string|max:255|unique:subjects',
+            'subject_code'  => 'required|numeric',
+            'teacher_id'    => 'required|numeric',
+            'description'   => 'required|string|max:255'
+        ]);
+
+        Subject::create([
+            'name'          => $request->name,
+            'slug'          => Str::slug($request->name),
+            'subject_code'  => $request->subject_code,
+            'teacher_id'    => $request->teacher_id,
+            'description'   => $request->description
+        ]);
+
+        return redirect()->route('subject.index');
+    }
+
+
+    public function show(Subject $subject)
+    {
+        //
+    }
+
+
+    public function edit(Subject $subject)
+    {
+        $teachers = Teacher::latest()->get();
+
+        return view('backend.subjects.edit', compact('subject','teachers'));
+    }
+
+
+    public function update(Request $request, Subject $subject)
+    {
+        $request->validate([
+            'name'          => 'required|string|max:255|unique:subjects,name,'.$subject->id,
+            'subject_code'  => 'required|numeric',
+            'teacher_id'    => 'required|numeric',
+            'description'   => 'required|string|max:255'
+        ]);
+
+        $subject->update([
+            'name'          => $request->name,
+            'slug'          => Str::slug($request->name),
+            'subject_code'  => $request->subject_code,
+            'teacher_id'    => $request->teacher_id,
+            'description'   => $request->description
+        ]);
+
+        return redirect()->route('subject.index');
+    }
+
+
+    public function destroy(Subject $subject)
+    {
+        $subject->delete();
+
+        return back();
+    }
+}
