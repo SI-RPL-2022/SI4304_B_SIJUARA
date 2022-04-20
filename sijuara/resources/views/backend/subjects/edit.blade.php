4a9912c0e2018660803c98928f785e6dd2d27bfe
yadinugrahaa-github <skyres9@gmail.com>
2022-04-20 23:07:06 +0700

    Fix back End edit mata pelajaran #3
    Sebelumnya error karena class ada yg tidak terpanggil tapi sekarang udah bisa ditampilkan

--- sijuara/resources/views/backend/subjects/edit.blade.php
+++ sijuara/resources/views/backend/subjects/edit.blade.php
@@ -5,83 +5,84 @@
 
         <div class="flex items-center justify-between mb-6">
             <div>
-                <h2 class="text-gray-700 uppercase font-bold">Edit Class</h2>
+                <h2 class="text-gray-700 uppercase font-bold">Edit Kelas</h2>
             </div>
             <div class="flex flex-wrap items-center">
-                <a href="{{ route('classes.index') }}" class="bg-gray-200 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
+                <a href="{{ route('subject.index') }}" class="bg-gray-200 text-gray-700 text-sm uppercase py-2 px-4 flex items-center rounded">
                     <svg class="w-3 h-3 fill-current" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-left" class="svg-inline--fa fa-long-arrow-alt-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"></path></svg>
-                    <span class="ml-2 text-xs font-semibold">Back</span>
+                    <span class="ml-2 text-xs font-semibold">Kembali</span>
                 </a>
             </div>
         </div>
 
         <div class="table w-full mt-8 bg-white rounded">
-            <form action="{{ route('classes.update',$class->id) }}" method="POST" class="w-full max-w-xl px-6 py-12">
-                
+            <form action="{{ route('subject.update',$subject->id) }}" method="POST" class="w-full max-w-xl px-6 py-12">
+                @csrf
+                @method('PUT')
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Nama Kelas
+                            Nama Mata pelajaran
                         </label>
                     </div>
                     <div class="md:w-2/3">
-                        <input name="class_name" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $class->class_name }}">
-                            
-                            <p class="text-red-500 text-xs italic"></p>
-                        
+                        <input name="name" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $subject->name }}">
+                        @error('name')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Kode Kelas
+                            Kode Mata pelajaran
                         </label>
                     </div>
                     <div class="md:w-2/3">
-                        <input name="class_numeric" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="number" value="{{ $class->class_numeric }}">
-                            
-                            <p class="text-red-500 text-xs italic"></p>
-                        
+                        <input name="subject_code" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="number" value="{{ $subject->subject_code }}">
+                        @error('subject_code')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                         <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Pilih Guru
+                            Deskripsi Mata Pelajaran
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input name="description" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $subject->description }}">
+                        @error('description')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Pilih Guru pengampu
                         </label>
                     </div>
                     <div class="md:w-2/3 block text-gray-600 font-bold">
                         <div class="relative">
                             <select name="teacher_id" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                                 <option value="">--Pilih Guru--</option>
-                                     
+                                @foreach ($teachers as $teacher)
                                     <option value="{{ $teacher->id }}"
-                                        
+                                        {{ ($teacher->id === $subject->teacher_id) ? 'selected' : '' }}
                                     >
-                                        
+                                        {{ $teacher->user->name }}
                                     </option>
-                                
+                                @endforeach
                             </select>
                             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
                             </div>
                         </div>
-                            
-                            <p class="text-red-500 text-xs font-normal italic"></p>
-                        
-                    </div>
-                </div>
-                <div class="md:flex md:items-center mb-6">
-                    <div class="md:w-1/3">
-                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                            Keterangan Kelas
-                        </label>
-                    </div>
-                    <div class="md:w-2/3">
-                        <input name="class_description" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $class->class_description }}">
-                            
-                            <p class="text-red-500 text-xs italic">{</p>
-                        
+                        @error('teacher_id')
+                            <p class="text-red-500 text-xs font-normal italic">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
 
@@ -89,7 +90,7 @@
                     <div class="md:w-1/3"></div>
                     <div class="md:w-2/3">
                         <button class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
-                            Update Kelas
+                           Update Mata Pelajaran
                         </button>
                     </div>
                 </div>
