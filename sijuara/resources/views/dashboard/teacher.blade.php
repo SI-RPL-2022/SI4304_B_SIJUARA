a39f209ff3b0424b319d5e86c096190f1c92dd82
Shazkia Septiaweni <87314321+shazkiaseptiaweni@users.noreply.github.com>
2022-05-30 00:46:46 +0700

    Update Dashboard Teacher #13

--- sijuara/resources/views/dashboard/teacher.blade.php
+++ sijuara/resources/views/dashboard/teacher.blade.php
@@ -2,19 +2,19 @@
     <div class="flex flex-wrap sm:flex-no-wrap justify-between">
         <div class="w-full bg-gray-200 text-center border border-gray-300 px-8 py-6 rounded">
             <h3 class="text-gray-700 uppercase font-bold">
-                <span class="text-4xl">{{ sprintf("%02d", $teacher->classes_count) }}</span>
+                <span class="text-4xl"></span>
                 <span class="leading-tight">Kelas </span>
             </h3>
         </div>
         <div class="w-full bg-gray-200 text-center border border-gray-300 px-8 py-6 mx-0 sm:mx-6 my-4 sm:my-0 rounded">
             <h3 class="text-gray-700 uppercase font-bold">
-                <span class="text-4xl">{{ sprintf("%02d", $teacher->subjects_count) }}</span>
+                <span class="text-4xl"></span>
                 <span class="leading-tight">Mata Pelajaran </span>
             </h3>
         </div>
         <div class="w-full bg-gray-200 text-center border border-gray-300 px-8 py-6 rounded">
             <h3 class="text-gray-700 uppercase font-bold">
-                <span class="text-4xl">{{ ($teacher->students[0]->students_count) ?? 0 }}</span>
+                <span class="text-4xl"></span>
                 <span class="leading-tight">Siswa </span>
             </h3>
         </div>
@@ -25,12 +25,10 @@
         <div class="w-full sm:w-1/2 mr-2 mb-6">
             <h3 class="text-gray-700 uppercase font-bold mb-2">Daftar Kelas</h3>
             <div class="flex flex-wrap items-center">
-                @foreach ($teacher->classes as $class)
                     <div class="w-full sm:w-1/2 text-center border border-gray-200 rounded">
-                        <div class="text-gray-800 uppercase font-semibold px-4 py-4 mb-2">{{ $class->class_name }}</div>
-                        <a href="{{ route('teacher.attendance.create',$class->id) }}" class="bg-gray-200 inline-block mb-4 text-xs text-gray-600 uppercase font-semibold px-4 py-2 border border-gray-200 rounded">Kehadiran</a>
+                        <div class="text-gray-800 uppercase font-semibold px-4 py-4 mb-2"></div>
+                        <a href="" class="bg-gray-200 inline-block mb-4 text-xs text-gray-600 uppercase font-semibold px-4 py-2 border border-gray-200 rounded">Kehadiran</a>
                     </div>
-                @endforeach
             </div>
         </div>
         <div class="w-full sm:w-1/2 ml-2 mb-6">
@@ -40,13 +38,11 @@
                 <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-semibold">Nama MatPel</div>
                 <div class="w-1/3 text-right text-gray-600 py-2 px-4 font-semibold">Guru Pengampu</div>
             </div>
-            @foreach ($teacher->subjects as $subject)
                 <div class="flex items-center justify-between border border-gray-200">
-                    <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-medium">{{ $subject->subject_code }}</div>
-                    <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-medium">{{ $subject->name }}</div>
-                    <div class="w-1/3 text-right text-gray-600 py-2 px-4 font-medium">{{ $subject->teacher->user->name }}</div>
+                    <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-medium"></div>
+                    <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-medium"></div>
+                    <div class="w-1/3 text-right text-gray-600 py-2 px-4 font-medium"></div>
                 </div>
-            @endforeach
         </div>
     </div>
-</div> <!-- ./END teacher -->
+</div>
