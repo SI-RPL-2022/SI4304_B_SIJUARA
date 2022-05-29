4f65a2b25d63bc663eddc6808125e1106bd7c04c
gelafra <gelafrasurbakti@gmail.com>
2022-05-29 23:09:21 +0700

    Edit dashboard admin
    Mengubah tampilan nama pada dashboard admin #12

--- sijuara/resources/views/dashboard/admin.blade.php
+++ sijuara/resources/views/dashboard/admin.blade.php
@@ -3,19 +3,19 @@
         <div class="w-full bg-blue-300 text-center border border-blue-300 px-10 py-6 rounded">
             <h3 class="text-white-700 uppercase font-bold">
                 <span class="text-4xl">{{ sprintf("%02d", count($parents)) }}</span>
-                <span class="leading-tight">Parents</span>
+                <span class="leading-tight">Orang Tua</span>
             </h3>
         </div>
         <div class="w-full bg-green-300 text-center border border-green-300 px-10 py-6 mx-0 sm:mx-6 my-4 sm:my-0 rounded">
             <h3 class="text-white-700 uppercase font-bold">
                 <span class="text-4xl">{{ sprintf("%02d", count($teachers)) }}</span>
-                <span class="leading-tight">Teachers</span>
+                <span class="leading-tight">Guru</span>
             </h3>
         </div>
         <div class="w-full bg-pink-400 text-center border border-pink-300 px-10 py-6 rounded">
             <h3 class="text-white-700 uppercase font-bold">
                 <span class="text-4xl">{{ sprintf("%02d", count($students)) }}</span>
-                <span class="leading-tight">Students</span>
+                <span class="leading-tight">Siswa</span>
             </h3>
         </div>
     </div>
