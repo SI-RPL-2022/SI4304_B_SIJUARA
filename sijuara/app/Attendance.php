05883139b055d610a11a5240023207556f3ce703
yadinugrahaa-github <skyres9@gmail.com>
2022-05-29 21:12:47 +0700

    Update controller #12

---
+++ sijuara/app/Attendance.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+}
