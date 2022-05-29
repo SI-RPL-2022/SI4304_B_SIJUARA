8a0704785421260941143b0d13f483f32276ecd0
yadinugrahaa-github <skyres9@gmail.com>
2022-05-29 21:26:15 +0700

    Fix controller siswa #12

--- sijuara/app/Attendance.php
+++ sijuara/app/Attendance.php
@@ -4,12 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Subject extends Model
+class Attendance extends Model
 {
+    protected $fillable = [
+        'class_id',
+        'teacher_id',
+        'student_id',
+        'attendence_date',
+        'attendence_status'
+    ];
 
+    public function student() {
+        return $this->belongsTo(Student::class);
+    }
 
-    public function teacher()
-    {
+    public function teacher() {
         return $this->belongsTo(Teacher::class);
+    }
+
+    public function class() {
+        return $this->belongsTo(Grade::class);
     }
 }
