90b4b295cd7c827fb9dc4ed7f9d5cf8867f0ae53
tiarahatig <tiarahatigiwangkara@gmail.com>
2022-05-29 22:32:41 +0700

    fixing dasboard student view #8

--- sijuara/resources/views/dashboard/student.blade.php
+++ sijuara/resources/views/dashboard/student.blade.php
@@ -4,7 +4,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Name : 
+                        Nama : 
                     </label>
                 </div>
                 <div class="md:w-2/3">
@@ -24,7 +24,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Roll Number :
+                    Nomor Urut Absen :
                     </label>
                 </div>
                 <div class="md:w-2/3">
@@ -34,7 +34,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Phone :
+                    Nomor HP :
                     </label>
                 </div>
                 <div class="md:w-2/3">
@@ -44,7 +44,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Gender :
+                        Jenis Kelamin :
                     </label>
                 </div>
                 <div class="md:w-2/3">
@@ -54,7 +54,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Date of Birth :
+                        Tanggal Lahir :
                     </label>
                 </div>
                 <div class="md:w-2/3">
@@ -64,7 +64,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Current Address :
+                        Alamat pada KK :
                     </label>
                 </div>
                 <div class="md:w-2/3">
@@ -74,7 +74,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Permanent Address :
+                        Alamat Tinggal :
                     </label>
                 </div>
                 <div class="md:w-2/3">
@@ -84,7 +84,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Class :
+                        Kelas :
                     </label>
                 </div>
                 <div class="md:w-2/3 block text-gray-600 font-bold">
@@ -94,7 +94,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Student Parent :
+                        Nama Orangtua/Wali :
                     </label>
                 </div>
                 <div class="md:w-2/3 block text-gray-600 font-bold">
@@ -104,7 +104,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Parent Email :
+                        Email Orangtua/Wali:
                     </label>
                 </div>
                 <div class="md:w-2/3 block text-gray-600 font-bold">
@@ -114,7 +114,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Parent Phone :
+                        No HP Orangtua/Wali :
                     </label>
                 </div>
                 <div class="md:w-2/3 block text-gray-600 font-bold">
@@ -124,7 +124,7 @@
             <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Parent Address :
+                        Alamat Orangtua/Wali :
                     </label>
                 </div>
                 <div class="md:w-2/3 block text-gray-600 font-bold">
@@ -134,9 +134,9 @@
 
             <div class="w-full px-0 md:px-6 py-12">
                 <div class="flex items-center bg-gray-200">
-                    <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-semibold">Code</div>
-                    <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-semibold">Subject</div>
-                    <div class="w-1/3 text-right text-gray-600 py-2 px-4 font-semibold">Teacher</div>
+                    <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-semibold">Kode</div>
+                    <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-semibold">Mata Pelajaran</div>
+                    <div class="w-1/3 text-right text-gray-600 py-2 px-4 font-semibold">Guru</div>
                 </div>
                 @foreach ($student->class->subjects as $subject)
                     <div class="flex items-center justify-between border border-gray-200 -mb-px">
@@ -149,10 +149,10 @@
 
             <div class="w-full px-0 md:px-6 py-12">
                 <div class="flex items-center bg-gray-200">
-                    <div class="w-1/4 text-left text-gray-600 py-2 px-4 font-semibold">Date</div>
-                    <div class="w-1/4 text-left text-gray-600 py-2 px-4 font-semibold">Class</div>
-                    <div class="w-1/4 text-left text-gray-600 py-2 px-4 font-semibold">Teacher</div>
-                    <div class="w-1/4 text-right text-gray-600 py-2 px-4 font-semibold">attendance</div>
+                    <div class="w-1/4 text-left text-gray-600 py-2 px-4 font-semibold">Tanggal</div>
+                    <div class="w-1/4 text-left text-gray-600 py-2 px-4 font-semibold">Kelas</div>
+                    <div class="w-1/4 text-left text-gray-600 py-2 px-4 font-semibold">Guru</div>
+                    <div class="w-1/4 text-right text-gray-600 py-2 px-4 font-semibold">Absen</div>
                 </div>
                 @foreach ($student->attendances as $attendance)
                     <div class="flex items-center justify-between border border-gray-200 -mb-px">
