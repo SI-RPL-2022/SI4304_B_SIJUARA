498b86f30d2e273047490d7fe9a7df38c3488624
tiarahatig <tiarahatigiwangkara@gmail.com>
2022-05-30 11:25:27 +0700

    Update user teacher view #10

--- sijuara/resources/views/dashboard/teacher.blade.php
+++ sijuara/resources/views/dashboard/teacher.blade.php
@@ -1,18 +1,18 @@
 <div class="w-full block mt-8">
     <div class="flex flex-wrap sm:flex-no-wrap justify-between">
-        <div class="w-full bg-gray-200 text-center border border-gray-300 px-8 py-6 rounded">
+        <div class="w-full bg-pink-200 text-center border border-gray-300 px-8 py-6 rounded">
             <h3 class="text-gray-700 uppercase font-bold">
                 <span class="text-4xl"></span>
                 <span class="leading-tight">Kelas </span>
             </h3>
         </div>
-        <div class="w-full bg-gray-200 text-center border border-gray-300 px-8 py-6 mx-0 sm:mx-6 my-4 sm:my-0 rounded">
+        <div class="w-full bg-blue-200 text-center border border-gray-300 px-8 py-6 mx-0 sm:mx-6 my-4 sm:my-0 rounded">
             <h3 class="text-gray-700 uppercase font-bold">
                 <span class="text-4xl"></span>
                 <span class="leading-tight">Mata Pelajaran </span>
             </h3>
         </div>
-        <div class="w-full bg-gray-200 text-center border border-gray-300 px-8 py-6 rounded">
+        <div class="w-full bg-green-200 text-center border border-gray-300 px-8 py-6 rounded">
             <h3 class="text-gray-700 uppercase font-bold">
                 <span class="text-4xl"></span>
                 <span class="leading-tight">Siswa </span>
