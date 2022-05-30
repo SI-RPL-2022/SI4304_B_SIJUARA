c76201f626c201f3285551198514c65f1132b8ce
tiarahatig <tiarahatigiwangkara@gmail.com>
2022-05-30 11:33:06 +0700

    fixing table teacher #10

--- sijuara/resources/views/dashboard/teacher.blade.php
+++ sijuara/resources/views/dashboard/teacher.blade.php
@@ -1,18 +1,18 @@
 <div class="w-full block mt-8">
     <div class="flex flex-wrap sm:flex-no-wrap justify-between">
-        <div class="w-full bg-pink-200 text-center border border-gray-300 px-8 py-6 rounded">
+        <div class="w-full bg-blue-300 text-center border border-gray-300 px-8 py-6 rounded">
             <h3 class="text-gray-700 uppercase font-bold">
                 <span class="text-4xl"></span>
                 <span class="leading-tight">Kelas </span>
             </h3>
         </div>
-        <div class="w-full bg-blue-200 text-center border border-gray-300 px-8 py-6 mx-0 sm:mx-6 my-4 sm:my-0 rounded">
+        <div class="w-full bg-purple-300 text-center border border-gray-300 px-8 py-6 mx-0 sm:mx-6 my-4 sm:my-0 rounded">
             <h3 class="text-gray-700 uppercase font-bold">
                 <span class="text-4xl"></span>
                 <span class="leading-tight">Mata Pelajaran </span>
             </h3>
         </div>
-        <div class="w-full bg-green-200 text-center border border-gray-300 px-8 py-6 rounded">
+        <div class="w-full bg-green-300 text-center border border-gray-300 px-8 py-6 rounded">
             <h3 class="text-gray-700 uppercase font-bold">
                 <span class="text-4xl"></span>
                 <span class="leading-tight">Siswa </span>
@@ -33,10 +33,10 @@
         </div>
         <div class="w-full sm:w-1/2 ml-2 mb-6">
             <h3 class="text-gray-700 uppercase font-bold mb-2">Daftar Mata Pelajaran</h3>
-            <div class="flex items-center bg-gray-200 rounded-tl rounded-tr">
-                <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-semibold">Kode</div>
-                <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-semibold">Nama MatPel</div>
-                <div class="w-1/3 text-right text-gray-600 py-2 px-4 font-semibold">Guru Pengampu</div>
+            <div class="flex items-center bg-pink-400 rounded-tl rounded-tr">
+                <div class="w-1/3 text-left text-dark-600 py-2 px-4 font-semibold">Kode</div>
+                <div class="w-1/3 text-left text-dark-600 py-2 px-4 font-semibold">Nama MatPel</div>
+                <div class="w-1/3 text-right text-dark-600 py-2 px-4 font-semibold">Guru Pengampu</div>
             </div>
                 <div class="flex items-center justify-between border border-gray-200">
                     <div class="w-1/3 text-left text-gray-600 py-2 px-4 font-medium"></div>
