89d55a83b525c7a32eccc8b90c9e082351600aa2
yadinugrahaa-github <skyres9@gmail.com>
2022-06-13 13:08:12 +0700

    Masih ada error di front end orang tua

--- sijuara/resources/views/dashboard/parents.blade.php
+++ sijuara/resources/views/dashboard/parents.blade.php
@@ -2,8 +2,8 @@
     <div class="flex flex-wrap sm:flex-no-wrap justify-between">
         <div class="w-full sm:max-w-sm bg-gray-200 text-center border border-gray-300 rounded px-8 py-6 my-4 sm:my-0">
             <h3 class="text-gray-700 uppercase font-bold">
-                <span class="text-4xl">{{ sprintf("%02d", $parents->children_count) }}</span>
-                <span class="leading-tight">Children</span>
+                <span class="text-4xl"></span>
+                <span class="leading-tight"></span>
             </h3>
         </div>
     </div>
@@ -11,37 +11,37 @@
 
 <div class="w-full block mt-4 sm:mt-8">
     <div class="flex flex-wrap sm:flex-no-wrap justify-between">
-        @foreach ($parents->children as $key => $children)
+       
             <div class="w-full bg-gray-200 text-center border border-gray-300 rounded px-8 py-6 mb-4 {{ ($key>=1) ? 'ml-0 sm:ml-2' : '' }} {{ ($parents->children_count===1) ? 'sm:max-w-sm' : '' }}">
                 <div class="text-gray-700 font-bold">
                     <div class="mb-6">
-                        <div class="text-lg uppercase">{{ $children->user->name }}</div>
-                        <div class="text-sm lowercase leading-tight">{{ $children->user->email }}</div>
+                        <div class="text-lg uppercase"></div>
+                        <div class="text-sm lowercase leading-tight"></div>
                     </div>
 
                     <div class="flex items-center justify-between">
-                        <div class="w-1/2 text-sm text-right">Class :</div>
-                        <div class="w-1/2 text-sm text-left ml-2">{{ $children->class->class_name }}</div>
+                        <div class="w-1/2 text-sm text-right"></div>
+                        <div class="w-1/2 text-sm text-left ml-2"></div>
                     </div>
                     <div class="flex items-center justify-between">
-                        <div class="w-1/2 text-sm text-right">Role :</div>
-                        <div class="w-1/2 text-sm text-left ml-2">{{ $children->roll_number }}</div>
+                        <div class="w-1/2 text-sm text-right"></div>
+                        <div class="w-1/2 text-sm text-left ml-2"></div>
                     </div>
                     <div class="flex items-center justify-between">
-                        <div class="w-1/2 text-sm text-right">Phone :</div>
-                        <div class="w-1/2 text-sm text-left ml-2">{{ $children->phone }}</div>
+                        <div class="w-1/2 text-sm text-right"></div>
+                        <div class="w-1/2 text-sm text-left ml-2"></div>
                     </div>
                     <div class="flex items-center justify-between">
-                        <div class="w-1/2 text-sm text-right">Gender :</div>
-                        <div class="w-1/2 text-sm text-left ml-2">{{ $children->gender }}</div>
+                        <div class="w-1/2 text-sm text-right"></div>
+                        <div class="w-1/2 text-sm text-left ml-2"></div>
                     </div>
                     <div class="flex items-center justify-between">
-                        <div class="w-1/2 text-sm text-right">Date of Birth :</div>
-                        <div class="w-1/2 text-sm text-left ml-2">{{ $children->dateofbirth }}</div>
+                        <div class="w-1/2 text-sm text-right"></div>
+                        <div class="w-1/2 text-sm text-left ml-2"></div>
                     </div>
                     <div class="flex items-center justify-between">
-                        <div class="w-1/2 text-sm text-right">Address :</div>
-                        <div class="w-1/2 text-sm text-left ml-2">{{ $children->current_address }}</div>
+                        <div class="w-1/2 text-sm text-right"></div>
+                        <div class="w-1/2 text-sm text-left ml-2"></div>
                     </div>
 
                     <div class="mt-6">
