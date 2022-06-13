d7b0fa04347727d66269e709e8194371c6686e9e
gelafra <gelafrasurbakti@gmail.com>
2022-06-13 13:42:07 +0700

    Ubah front end dasboard ortu #18
    Mengganti warna bar sesuai dengan dashboard pada admin

--- sijuara/resources/views/dashboard/parents.blade.php
+++ sijuara/resources/views/dashboard/parents.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full block mt-8">
     <div class="flex flex-wrap sm:flex-no-wrap justify-between">
-        <div class="w-full sm:max-w-sm bg-gray-200 text-center border border-gray-300 rounded px-8 py-6 my-4 sm:my-0">
+        <div class="w-full sm:max-w-sm bg-pink-200 text-center border border-pink-300 rounded px-8 py-6 my-4 sm:my-0">
             <h3 class="text-gray-700 uppercase font-bold">
                 <span class="text-4xl">{{ sprintf("%02d", $parents->children_count) }}</span>
                 <span class="leading-tight">Siswa</span>
@@ -12,7 +12,7 @@
 <div class="w-full block mt-4 sm:mt-8">
     <div class="flex flex-wrap sm:flex-no-wrap justify-between">
     @foreach ($parents->children as $key => $children)
-            <div class="w-full bg-gray-200 text-center border border-gray-300 rounded px-8 py-6 mb-4 {{ ($key>=1) ? 'ml-0 sm:ml-2' : '' }} {{ ($parents->children_count===1) ? 'sm:max-w-sm' : '' }}">
+            <div class="w-full bg-pink-200 text-center border border-gray-300 rounded px-8 py-6 mb-4 {{ ($key>=1) ? 'ml-0 sm:ml-2' : '' }} {{ ($parents->children_count===1) ? 'sm:max-w-sm' : '' }}">
                 <div class="text-gray-700 font-bold">
                     <div class="mb-6">
                         <div class="text-lg uppercase">{{ $children->user->name }}</div>
@@ -40,8 +40,8 @@
                         <div class="w-1/2 text-sm text-left ml-2">{{ $children->current_address }}</div>
                     </div>
                     <div class="mt-6">
-                        <a href="" class="bg-gray-100 inline-block mb-4 text-xs text-gray-600 uppercase font-semibold px-4 py-2 border border-gray-400 rounded">Tes1</a>
-                        <a href="" class="bg-gray-100 inline-block mb-4 text-xs text-gray-600 uppercase font-semibold px-4 py-2 border border-gray-400 rounded">Tes2</a>
+                        <a href="" class="bg-pink-100 inline-block mb-4 text-xs text-pink-600 uppercase font-semibold px-4 py-2 border border-pink-400 rounded">Tes1</a>
+                        <a href="" class="bg-pink-100 inline-block mb-4 text-xs text-pink-600 uppercase font-semibold px-4 py-2 border border-pink-400 rounded">Tes2</a>
                     </div>
                 </div>
             </div>
