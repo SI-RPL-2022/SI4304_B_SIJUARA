214a70d700f00a4b132d962c933b8681976f823a
rvnger <104014968+hikamlukman@users.noreply.github.com>
2022-06-19 22:08:21 +0700

    update parent #18
    menambahkan form untuk mengedit data orang tua

--- sijuara/resources/views/backend/parents/edit.blade.php
+++ sijuara/resources/views/backend/parents/edit.blade.php
@@ -14,6 +14,108 @@
                 </a>
             </div>
         </div>
+        <div class="table w-full mt-8 bg-white rounded">
+            <form action="{{ route('parents.update',$parent->id) }}" method="POST" class="w-full max-w-xl px-6 py-12" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3"></div>
+                    <div class="md:w-2/3">
+                        <img class="w-20 h-20 sm:w-32 sm:h-32 rounded" src="{{ asset('images/profile/' .$parent->user->profile_picture) }}" alt="avatar">
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Nama
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input name="name" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $parent->user->name }}">
+                        @error('name')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Alamat Email
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input name="email" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="email" value="{{ $parent->user->email }}">
+                        @error('email')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Nomor HP
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input name="phone" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $parent->phone }}">
+                        @error('phone')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Jenis Kelamin
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <div class="flex flex-row items-center">
+                            <label class="block text-gray-500 font-bold">
+                                <input name="gender" class="mr-2 leading-tight" type="radio" value="male" {{ ($parent->gender == 'male') ? 'checked' : '' }}>
+                                <span class="text-sm">Male</span>
+                            </label>
+                            <label class="ml-4 block text-gray-500 font-bold">
+                                <input name="gender" class="mr-2 leading-tight" type="radio" value="female" {{ ($parent->gender == 'female') ? 'checked' : '' }}>
+                                <span class="text-sm">Female</span>
+                            </label>
+                            <label class="ml-4 block text-gray-500 font-bold">
+                                <input name="gender" class="mr-2 leading-tight" type="radio" value="other" {{ ($parent->gender == 'other') ? 'checked' : '' }}>
+                                <span class="text-sm">Other</span>
+                            </label>
+                        </div>
+                        @error('gender')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Alamat Tinggal 
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input name="current_address" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $parent->current_address }}">
+                        @error('current_address')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Domisili Sekarang
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input name="permanent_address" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value="{{ $parent->permanent_address }}">
+                        @error('permanent_address')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
 
 
 @endsection
